6f4a912b861c461cbae17b4f6978242b21c94829
SwapnilMohite1 <swapnil.mohite@qloron.com>
2023-12-29 22:37:27 +0530

    added a port

--- src/main/java/com/springbootcicd/SpringbootcicdApplication.java
+++ src/main/java/com/springbootcicd/SpringbootcicdApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootcicdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootcicdApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootcicdApplication.class, args);
+    }
 
 }
--- src/main/java/com/springbootcicd/controller/DemoController.java
+++ src/main/java/com/springbootcicd/controller/DemoController.java
@@ -1,2 +1,13 @@
-package com.springbootcicd.controller;public class DemoController {
+package com.springbootcicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String demo(){
+        return "Springboot CI/CD";
+    }
 }

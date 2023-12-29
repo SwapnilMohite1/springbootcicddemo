06bb8ca178a8650cde926939a52ebd4ff579d551
SwapnilMohite1 <swapnil.mohite@qloron.com>
2023-12-29 23:04:58 +0530

    added a port

--- src/main/java/com/springbootcicd/controller/DemoController.java
+++ src/main/java/com/springbootcicd/controller/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
     @GetMapping("/hello")
     public String demo(){
-        return "Springboot CI/CD is now working";
+        return "Springboot CI/CD is now working fine";
     }
 }
